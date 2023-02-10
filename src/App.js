f1c396d17e16f265d039548ac6b33d5dca95a6b1
Kristin Lam <klam623@gmail.com>
2023-02-09 23:26:13 -0500

    Add ability to create books

--- src/App.js
+++ src/App.js
@@ -1,5 +1,18 @@
+import { useState } from 'react';
+import BookCreate from './components/BookCreate';
+
 const App = () => {
-  return <div>App</div>;
+  const [books, setBooks] = useState([]);
+
+  const createBook = (title) => {
+    setBooks([...books, { id: Math.round(Math.random() * 9999), title }]);
+  };
+
+  return (
+    <div>
+      <BookCreate onCreate={createBook} />
+    </div>
+  );
 };
 
 export default App;
