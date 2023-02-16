64d455fea4b5c362c1384c8917bf8b1255fa7e53
Kristin Lam <klam623@gmail.com>
2023-02-15 22:50:38 -0500

    Create context for books and refactor App

--- src/App.js
+++ src/App.js
@@ -1,49 +1,20 @@
-import { useState, useEffect } from 'react';
-import axios from 'axios';
+import { useEffect, useContext } from 'react';
 import BookCreate from './components/BookCreate';
 import BookList from './components/BookList';
+import BooksContext from './context/books';
 
 const App = () => {
-  const [books, setBooks] = useState([]);
-
-  const fetchBooks = async () => {
-    const response = await axios.get('http://localhost:3001/books');
-    setBooks(response.data);
-  };
+  const { fetchBooks } = useContext(BooksContext);
 
   useEffect(() => {
     fetchBooks();
   }, []);
 
-  const createBook = async (title) => {
-    const response = await axios.post('http://localhost:3001/books', { title });
-    setBooks([...books, response.data]);
-  };
-
-  const deleteBookById = async (id) => {
-    const response = await axios.delete(`http://localhost:3001/books/${id}`);
-
-    const filteredBooks = books.filter((book) => book.id !== id);
-    setBooks(filteredBooks);
-  };
-
-  const editBookById = async (id, newTitle) => {
-    const response = await axios.put(`http://localhost:3001/books/${id}`, {
-      title: newTitle,
-    });
-
-    const updatedArray = books.map((book) => {
-      if (book.id === id) return { ...book, ...response.data };
-      else return book;
-    });
-    setBooks(updatedArray);
-  };
-
   return (
     <div className="app">
       <h1>Lore List</h1>
-      <BookCreate onCreate={createBook} />
-      <BookList books={books} onDelete={deleteBookById} onEdit={editBookById} />
+      <BookCreate />
+      <BookList />
     </div>
   );
 };
--- src/index.js
+++ src/index.js
@@ -1,6 +1,11 @@
 import './index.css';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import { Provider } from './context/books';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<App />);
+root.render(
+  <Provider>
+    <App />
+  </Provider>
+);
